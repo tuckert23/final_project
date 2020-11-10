6252a0cd571ae78c826aed8c1eea434163a797bd
tuckert23 <tuckert23@mail.wlu.edu>
2020-11-09 19:12:36 -0500

    made eda a separate file, added new feature (Percent Men), and commented on EDA results

---
+++ eda.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mlxtend.plotting import scatterplotmatrix
+from mlxtend.plotting import heatmap
+
+df = pd.read_csv("./cleaned_data_final.csv")
+
+columns = df.columns
+
+# Scatterplot Matrix
+scatterplotmatrix(df[columns].values, figsize=(30, 30), names=columns, alpha=0.8)
+plt.tight_layout()
+plt.savefig("./figs/scatterplot_matrix.png")
+
+#Correlation heatmap
+corrmap = np.corrcoef(df[columns].values.T)
+hmap = heatmap(corrmap, row_names=columns, column_names=columns, figsize=(20, 20))
+plt.savefig("./figs/heatmap.png")
+
