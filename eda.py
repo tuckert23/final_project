f6cad8ddaa714a62bf6a868e2bb6364a9fe0dc20
0oTinaKimo0 <0oTinaKimo0@users.noreply.github.com>
2020-11-09 20:56:04 -0500

    Getting started on feature selection

--- eda.py
+++ eda.py
@@ -13,7 +13,7 @@ scatterplotmatrix(df[columns].values, figsize=(30, 30), names=columns, alpha=0.8
 plt.tight_layout()
 plt.savefig("./figs/scatterplot_matrix.png")
 
-#Correlation heatmap
+# Correlation heatmap
 corrmap = np.corrcoef(df[columns].values.T)
 hmap = heatmap(corrmap, row_names=columns, column_names=columns, figsize=(20, 20))
 plt.savefig("./figs/heatmap.png")
---
+++ scaling_selection.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectFromModel
+
+df = pd.read_csv("./cleaned_data_final.csv")
+X, y = df.iloc[:, 0:-1].values, df.iloc[:, -1].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Normalizing the features to [0,1] using MinMaxScaler
+norm = MinMaxScaler()
+X_train_norm = norm.fit_transform(X_train)
+X_test_norm = norm.transform(X_test)
+
+# Standardizing the features using StandardScaler
+std = StandardScaler()
+X_train_std = std.fit_transform(X_train)
+X_test_std = std.transform(X_test)
+
+features = df.columns[0:-1]
+
+forest = RandomForestRegressor(n_estimators=500, random_state=1)
+
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+
+indices = np.argsort(importances)[::-1]
+
+for f in range(X_train.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, 
+                            features[indices[f]],
+                            importances[indices[f]]))
+
+plt.title('Feature Importance')
+plt.bar(range(X_train.shape[1]), 
+        importances[indices],
+        align='center')
+
+plt.xticks(range(X_train.shape[1]), 
+           features[indices], rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.savefig('figs/FeatureImportances.png', dpi=300)
+
+sfm = SelectFromModel(forest, threshold=0.01, prefit=True)
+X_selected = sfm.transform(X_train)
+print('Number of features that meet threshold of 0.01 is', X_selected.shape[1], 'including')
+for f in range(X_selected.shape[1]):
+    print("%2d) %-*s %f" % (f + 1, 30, features[indices[f]], importances[indices[f]]))
